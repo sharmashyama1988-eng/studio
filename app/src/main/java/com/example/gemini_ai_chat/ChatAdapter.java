76d7fc13f673e47e5e3fa257f9d072838383841a
Sharma Shayama <sharmashyama1988@gmail.com>
2026-02-01 14:49:33 +0000

    new

--- app/src/main/java/com/example/gemini_ai_chat/ChatAdapter.java
+++ app/src/main/java/com/example/gemini_ai_chat/ChatAdapter.java
@@ -1,37 +1,32 @@
 
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.gemini_ai_chat.databinding.ListItemChatMessageBinding;
-
 import java.util.List;
 
-public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MessageViewHolder> {
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
 
-    private List<Message> messages;
+    private final List<ChatMessage> messages;
 
-    public ChatAdapter(List<Message> messages) {
+    public ChatAdapter(List<ChatMessage> messages) {
         this.messages = messages;
     }
 
     @NonNull
     @Override
-    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        ListItemChatMessageBinding binding = ListItemChatMessageBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-        return new MessageViewHolder(binding);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_chat_message, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
-        Message message = messages.get(position);
-        holder.bind(message);
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ChatMessage message = messages.get(position);
+        holder.senderTextView.setText(message.sender);
+        holder.messageTextView.setText(message.message);
     }
 
     @Override
@@ -39,30 +34,14 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MessageViewHol
         return messages.size();
     }
 
-    public void setMessages(List<Message> messages) {
-        this.messages = messages;
-        notifyDataSetChanged();
-    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView senderTextView;
+        public TextView messageTextView;
 
-    static class MessageViewHolder extends RecyclerView.ViewHolder {
-        private final ListItemChatMessageBinding binding;
-
-        public MessageViewHolder(ListItemChatMessageBinding binding) {
-            super(binding.getRoot());
-            this.binding = binding;
-        }
-
-        public void bind(Message message) {
-            binding.messageText.setText(message.text);
-            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) binding.messageText.getLayoutParams();
-            if (message.isUser) {
-                params.gravity = Gravity.END;
-                binding.messageText.setBackgroundResource(R.drawable.user_message_background);
-            } else {
-                params.gravity = Gravity.START;
-                binding.messageText.setBackgroundResource(R.drawable.ai_message_background);
-            }
-            binding.messageText.setLayoutParams(params);
+        public ViewHolder(View itemView) {
+            super(itemView);
+            senderTextView = itemView.findViewById(R.id.sender_text_view);
+            messageTextView = itemView.findViewById(R.id.message_text_view);
         }
     }
 }
